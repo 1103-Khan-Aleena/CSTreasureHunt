f93874e2f776a2190f0ee47a74f2c1a3ed6db813
Aleena Khan <toutetrakhan@gmail.com>
2025-05-04 01:48:01 -0700

    Add files via upload

--- helpers.cpp
+++ helpers.cpp
@@ -2,92 +2,112 @@
 #include <dirent.h>
 
 int generateRandomInt(){
-    int generatedNumber = (rand() % 4) + 1; //Because i want an interval between 0 and 4, using the remainder limits its range
+    int generatedNumber = (rand() % 4) + 1; // Because I want an interval between 0 and 4, using the remainder limits its range
     return generatedNumber;
 }
 
 bool checkGameStatus(Player& player){
-    if(player.getHealth()<=0){
+    if (player.getHealth() <= 0){
         cout << endl << "Game Over! You Died!\n" << endl;
-        return true;//ends the game
+        return true; // Ends the game
     }
-    if(player.getHealth()>=1 && player.getTreasures()==3){
-        cout << endl << "Congratulations! You colllected 3 treaures" << endl << "You Won!" << endl;
+    if (player.getHealth() >= 1 && player.getTreasures() == 3){
+        cout << endl << "Congratulations! You collected 3 treasures" << endl << "You Won!" << endl;
         return true;
     }
-    return false;// continues the game 
+    return false; // Continues the game 
 }
-//extra credit idk if this will even work directly copied and pasted from PA2 
+
+// Extra credit idk if this will even work copied directly from PA2!!
+#include "helpers.h"
+#include <dirent.h>
+
 int loadLeaderboardFromUserFiles(string names[], int wins[]) {
     DIR *directoryPtr = opendir(".");
     struct dirent *directoryEntry;
     int numPlayers = 0;
-    
+
     if (directoryPtr) {
         while ((directoryEntry = readdir(directoryPtr)) != NULL && numPlayers < 10) {
             string filename = directoryEntry->d_name;
-            if (filename.size() >= 4 && filename.substr(filename.size() - 4) == ".txt") {
-                if (filename != "leaderboard.txt") {
-                    // remove ".txt" extension
-                    string username = filename.substr(0, filename.size() - 4);
-                    
-                    // open the file
-                    ifstream file(filename);
-                    string line, lastLine;
-                    while (getline(file, line)) {
-                        lastLine = line; // keep updating until we reach last line
-                    }
-                    file.close();
-                    
-                    int userWins = 0;
-                    if (!lastLine.empty()) {
-                    bool isValidNumber = true;
-                    userWins = 0;
 
-                // Loop through each character of the string and convert it to an integer
-                    for (int i = 0; i < lastLine.length(); i++) {
+            // Manually check if filename ends with ".txt" not using substr
+            bool isTxtFile = true;
+            int filenameLength = 0;
+            while (filename[filenameLength] != '\0') {
+                filenameLength++;
+            }
+
+            if (filenameLength >= 4) {
+                for (int i = 0; i < 4; i++) {
+                    if (filename[filenameLength - 4 + i] != ".txt"[i]) {
+                        isTxtFile = false;
+                        break;
+                    }
+                }
+            } 
+            else {
+                isTxtFile = false;//is this a text file or no
+            }
+
+            if (isTxtFile && filename != "leaderboard.txt") {
+                // Manually remove ".txt" from the filename
+                string username = "";
+                for (int i = 0; i < filenameLength - 4; i++) {
+                    username += filename[i];
+                }
+
+                // Open the file
+                ifstream file(filename);
+                string line, lastLine;
+                while (getline(file, line)) {
+                    lastLine = line; // Keep updating until we reach the last line
+                }
+                file.close();
+
+                int userWins = 0;
+                // Check if lastLine is not empty 
+                bool isValidNumber = true;
+                if (lastLine[0] != '\0') { // Check if it's not empty
+                    userWins = 0;
+                    for (int i = 0; lastLine[i] != '\0'; i++) {
                         if (lastLine[i] < '0' || lastLine[i] > '9') {
                             isValidNumber = false;
                             break;
                         }
-
-        // Update the number by multiplying the current result by 10 and adding the digit
-        userWins = userWins * 10 + (lastLine[i] - '0');
-    }
-
-    // If the string was invalid (non-digit characters), set userWins to 0
-    if (!isValidNumber) {
-        userWins = 0;
-    }
-}
-        names[numPlayers] = username;
-        wins[numPlayers] = userWins;
-        numPlayers++;
+                        userWins = userWins * 10 + (lastLine[i] - '0');
+                    }
                 }
+
+                // If the string was invalid (non-digit characters), set userWins to 0
+                if (!isValidNumber) {
+                    userWins = 0;
+                }
+
+                names[numPlayers] = username;
+                wins[numPlayers] = userWins;
+                numPlayers++;
             }
         }
         closedir(directoryPtr);
     }
-    
-    // sort leaderboard
-    for (int i = 0; i < numPlayers - 1; i++) {
-    for (int j = i + 1; j < numPlayers; j++) {
-        if (wins[j] > wins[i]) {
-            // Swap wins[i] and wins[j] manually
-            int tempWins = wins[i];
-            wins[i] = wins[j];
-            wins[j] = tempWins;
 
-            // Swap names[i] and names[j] manually
-            string tempName = names[i];
-            names[i] = names[j];
-            names[j] = tempName;
+    // Sort leaderboard from the highest nunber to the then the lowest
+    for (int i = 0; i < numPlayers - 1; i++) {
+        for (int j = i + 1; j < numPlayers; j++) {
+            if (wins[j] > wins[i]) {
+                //Swap wins[i] and wins[j] manually
+                int tempWins = wins[i];
+                wins[i] = wins[j];
+                wins[j] = tempWins;
+
+                //Swap names[i] and names[j] manually
+                string tempName = names[i];
+                names[i] = names[j];
+                names[j] = tempName;
+            }
         }
     }
+
+    return numPlayers;
 }
-return numPlayers;
-}
-
-
-
-
