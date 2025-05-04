3e15e0b7157325f39bfc40b8529e18569c109594
Aleena Khan <toutetrakhan@gmail.com>
2025-05-04 01:32:51 -0700

    Add files via upload

--- helpers.h
+++ helpers.h
@@ -4,8 +4,19 @@
 #include "time.h"
 #include "stdlib.h"
 #include "player.h"
+#include <iostream> 
+
 int generateRandomInt();
-//void updateAll(Player& p, BoardTiles&, UserBoard&);
 bool checkGameStatus(Player& player); //checks conditions for winning and losing
+//Template
+template <typename T>
+void clearBoard(T& board, int r, int c){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            board.setElementAtTile(i, j, ' ');
+        }
+    }
+}
+int loadLeaderboardFromUserFiles(string names[], int wins[]);// sorting of the directory (will lead to creation of leaderboard.txt)
 
 #endif
