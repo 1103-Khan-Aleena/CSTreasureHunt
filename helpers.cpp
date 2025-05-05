ca3264ecb9597538a1d2098043f3c62bce713af6
Aleena Khan <toutetrakhan@gmail.com>
2025-05-04 22:30:10 -0700

    Add files via upload
    comments and stoi added

--- helpers.cpp
+++ helpers.cpp
@@ -1,17 +1,17 @@
 #include "helpers.h"
 #include <dirent.h>
 
-int generateRandomInt(){
+int generateRandomInt() {
     int generatedNumber = (rand() % 4) + 1; // Because I want an interval between 0 and 4, using the remainder limits its range
     return generatedNumber;
 }
 
-bool checkGameStatus(Player& player){
-    if (player.getHealth() <= 0){
+bool checkGameStatus(Player& player) {
+    if (player.getHealth() <= 0) {
         cout << endl << "Game Over! You Died!\n" << endl;
         return true; // Ends the game
     }
-    if (player.getHealth() >= 1 && player.getTreasures() == 3){
+    if (player.getHealth() >= 1 && player.getTreasures() == 3) {
         cout << endl << "Congratulations! You collected 3 treasures" << endl << "You Won!" << endl;
         return true;
     }
@@ -19,12 +19,9 @@ bool checkGameStatus(Player& player){
 }
 
 // Extra credit idk if this will even work copied directly from PA2!!
-#include "helpers.h"
-#include <dirent.h>
-
 int loadLeaderboardFromUserFiles(string names[], int wins[]) {
-    DIR *directoryPtr = opendir(".");
-    struct dirent *directoryEntry;
+    DIR* directoryPtr = opendir("."); // open the current directory
+    struct dirent* directoryEntry; // ptr to hold each file
     int numPlayers = 0;
 
     if (directoryPtr) {
@@ -38,16 +35,15 @@ int loadLeaderboardFromUserFiles(string names[], int wins[]) {
                 filenameLength++;
             }
 
-            if (filenameLength >= 4) {
+            if (filenameLength >= 4) { // check extension if filename is more than 4 chars long
                 for (int i = 0; i < 4; i++) {
                     if (filename[filenameLength - 4 + i] != ".txt"[i]) {
                         isTxtFile = false;
                         break;
                     }
                 }
-            } 
-            else {
-                isTxtFile = false;//is this a text file or no
+            } else {
+                isTxtFile = false; // if filename is shorter than 4 its not a txt file
             }
 
             if (isTxtFile && filename != "leaderboard.txt") {
@@ -63,45 +59,46 @@ int loadLeaderboardFromUserFiles(string names[], int wins[]) {
                 while (getline(file, line)) {
                     lastLine = line; // Keep updating until we reach the last line
                 }
-                file.close();
+                file.close(); // close the file
 
                 int userWins = 0;
                 // Check if lastLine is not empty 
-                bool isValidNumber = true;
+                bool isValidNumber = true; // assume valid until its not
                 if (lastLine[0] != '\0') { // Check if it's not empty
                     userWins = 0;
                     for (int i = 0; lastLine[i] != '\0'; i++) {
-                        if (lastLine[i] < '0' || lastLine[i] > '9') {
+                        if (lastLine[i] < '0' || lastLine[i] > '9') { // if any char is not a num digit
                             isValidNumber = false;
                             break;
                         }
-                        userWins = userWins * 10 + (lastLine[i] - '0');
+                    }
+
+                    // If the string was invalid (nondigit characters), set userWins to 0
+                    if (isValidNumber) {
+                        userWins = stoi(lastLine); // to convert to int
+                    } else {
+                        userWins = 0; // fallback if the last line isn't a valid number
                     }
                 }
 
-                // If the string was invalid (non-digit characters), set userWins to 0
-                if (!isValidNumber) {
-                    userWins = 0;
-                }
-
-                names[numPlayers] = username;
-                wins[numPlayers] = userWins;
-                numPlayers++;
+                names[numPlayers] = username; // store into names array
+                wins[numPlayers] = userWins; // store into wins array
+                numPlayers++; // increment num of players found
             }
         }
         closedir(directoryPtr);
     }
 
-    // Sort leaderboard from the highest nunber to the then the lowest
+    // Sort leaderboard from the highest number to the then the lowest
     for (int i = 0; i < numPlayers - 1; i++) {
         for (int j = i + 1; j < numPlayers; j++) {
             if (wins[j] > wins[i]) {
-                //Swap wins[i] and wins[j] manually
+                // Swap wins[i] and wins[j] manually
                 int tempWins = wins[i];
                 wins[i] = wins[j];
                 wins[j] = tempWins;
 
-                //Swap names[i] and names[j] manually
+                // Swap names[i] and names[j] manually
                 string tempName = names[i];
                 names[i] = names[j];
                 names[j] = tempName;
@@ -109,5 +106,5 @@ int loadLeaderboardFromUserFiles(string names[], int wins[]) {
         }
     }
 
-    return numPlayers;
+    return numPlayers; // return how many players were loaded
 }
